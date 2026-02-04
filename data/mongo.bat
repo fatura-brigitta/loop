dfba5329a057febc6ef2a51705abc93f1cc70dff
fatura-brigitta <fatura.brigitta@students.jedlik.eu>
2026-02-04 19:25:23 +0100

    jsonok es bat

--- data/mongo.bat
+++ data/mongo.bat
@@ -1,8 +1,15 @@
 chcp 65001
-mongoimport --uri="mongodb+srv://projekt:projekt12345@cluster0.ultaiwi.mongodb.net/?appName=Cluster0" --db=service --collection=api_tokens --drop --file=api_tokens.json --jsonArray
-mongoimport --uri="mongodb+srv://projekt:projekt12345@cluster0.ultaiwi.mongodb.net/?appName=Cluster0" --db=service --collection=service_usages --drop --file=service_usages.json --jsonArray
-mongoimport --uri="mongodb+srv://projekt:projekt12345@cluster0.ultaiwi.mongodb.net/?appName=Cluster0" --db=service --collection=services --drop --file=services.json --jsonArray
-mongoimport --uri="mongodb+srv://projekt:projekt12345@cluster0.ultaiwi.mongodb.net/?appName=Cluster0" --db=service --collection=users --drop --file=users.json --jsonArray
-mongoimport --uri="mongodb+srv://projekt:projekt12345@cluster0.ultaiwi.mongodb.net/?appName=Cluster0" --db=service --collection=workspaces --drop --file=workspaces.json --jsonArray
+mongoimport --uri="mongodb+srv://projekt:projekt12345@cluster0.ultaiwi.mongodb.net/?appName=Cluster0" --db=loop --collection=admins --drop --file=admins.json --jsonArray
+mongoimport --uri="mongodb+srv://projekt:projekt12345@cluster0.ultaiwi.mongodb.net/?appName=Cluster0" --db=loop --collection=chairs --drop --file=chairs.json --jsonArray
+mongoimport --uri="mongodb+srv://projekt:projekt12345@cluster0.ultaiwi.mongodb.net/?appName=Cluster0" --db=loop --collection=discounts --drop --file=discounts.json --jsonArray
+mongoimport --uri="mongodb+srv://projekt:projekt12345@cluster0.ultaiwi.mongodb.net/?appName=Cluster0" --db=loop --collection=forums --drop --file=forums.json --jsonArray
+mongoimport --uri="mongodb+srv://projekt:projekt12345@cluster0.ultaiwi.mongodb.net/?appName=Cluster0" --db=loop --collection=halls --drop --file=halls.json --jsonArray
+mongoimport --uri="mongodb+srv://projekt:projekt12345@cluster0.ultaiwi.mongodb.net/?appName=Cluster0" --db=loop --collection=movies --drop --file=movies.json --jsonArray
+mongoimport --uri="mongodb+srv://projekt:projekt12345@cluster0.ultaiwi.mongodb.net/?appName=Cluster0" --db=loop --collection=ranks --drop --file=ranks.json --jsonArray
+mongoimport --uri="mongodb+srv://projekt:projekt12345@cluster0.ultaiwi.mongodb.net/?appName=Cluster0" --db=loop --collection=screening_types --drop --file=screening_types.json --jsonArray
+mongoimport --uri="mongodb+srv://projekt:projekt12345@cluster0.ultaiwi.mongodb.net/?appName=Cluster0" --db=loop --collection=screenings --drop --file=screenings.json --jsonArray
+mongoimport --uri="mongodb+srv://projekt:projekt12345@cluster0.ultaiwi.mongodb.net/?appName=Cluster0" --db=loop --collection=ticket_types --drop --file=ticket_types.json --jsonArray
+mongoimport --uri="mongodb+srv://projekt:projekt12345@cluster0.ultaiwi.mongodb.net/?appName=Cluster0" --db=loop --collection=tickets --drop --file=tickets.json --jsonArray
+mongoimport --uri="mongodb+srv://projekt:projekt12345@cluster0.ultaiwi.mongodb.net/?appName=Cluster0" --db=loop --collection=users --drop --file=users.json --jsonArray
 echo PLEASE KILL AND RESTART YOUR BACKEND SERVER DEV TASK IF RUNNING!
 pause

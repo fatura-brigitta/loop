c22e82c7ad5220997f7cd8652edf5dfd5d7b435f
fatura-brigitta <fatura.brigitta@students.jedlik.eu>
2026-02-26 23:14:09 +0100

    forum javitas

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -6,6 +6,7 @@ import bcrypt from "bcryptjs";
 import { cookies } from "next/headers";
 import { generate4DigitCode, codeExpiry } from "@/lib/emailVerification";
 import { sendVerificationEmail } from "@/lib/sendVerificationEmail";
+import { parsePhoneNumberFromString } from 'libphonenumber-js';
 
 /**
  * GET /api/auth

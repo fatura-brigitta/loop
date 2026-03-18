92d089eabc4ae1668fbf345577d5583575dffe62
fatura-brigitta <fatura.brigitta@students.jedlik.eu>
2026-03-18 09:08:46 +0100

    Try

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1303,7 +1303,7 @@ export default function AdminPage() {
                 <h2 className="font-semibold mb-4">Általános nyitvatartás</h2>
                 {openingHours.map((d, i) => (
 
-                  <div key={i} className="grid grid-cols-[70px_1fr_1fr] gap-2 mb-3 items-center">
+                  <div key={i} className="grid grid-cols-[70px_1fr_1fr] gap-2 mb-3">
 
                     <div className="text-sm">{days[d.weekday]}</div>
 
@@ -1318,7 +1318,7 @@ export default function AdminPage() {
                         setOpeningHours(copy)
 
                       }}
-                      className="w-full bg-slate-900 border border-white/10 px-2 py-1 rounded"
+                      className="bg-slate-900 border border-white/10 px-2 py-1 rounded"
                     />
 
                     <input
@@ -1332,7 +1332,7 @@ export default function AdminPage() {
                         setOpeningHours(copy)
 
                       }}
-                      className="w-full bg-slate-900 border border-white/10 px-2 py-1 rounded"
+                      className="bg-slate-900 border border-white/10 px-2 py-1 rounded"
                     />
 
                     {/* <input
